735ef4e0cb921c7d09ade63c58c6405461339e6d
San Chua <san.chua@iotamedtech.com>
2022-09-29 23:17:36 +0800

    added some field for story form

--- home/models.py
+++ home/models.py
@@ -18,11 +18,19 @@ class profile(models.Model):
         ('F', 'Female'),
         ('O','Others')
     )
+    TRUE_FALSE_CHOICES = (
+    (True, 'Yes'),
+    (False, 'No')
+    )
     name = models.CharField(max_length=200)
     email = models.CharField(max_length=200)
     socialmedia = models.CharField(max_length=200)
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
     age = models.IntegerField()
+    nationality = models.CharField(max_length=200)
+    language = models.CharField(max_length=200)
     travel_itinerary = models.CharField(max_length=1000)
     about_yourself = models.CharField(max_length=100)
+    pal_gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    pal_nationality = models.CharField(max_length=1, choices=TRUE_FALSE_CHOICES)
     #part to upload picture, think of video face recognition
