396c9e5f2e934708718ff374895ebf920ecd9431
San Chua <san.chua@iotamedtech.com>
2022-10-09 17:05:07 +0800

    created user register page

--- findyourpalapp/urls.py
+++ findyourpalapp/urls.py
@@ -1,9 +1,11 @@
 from . import views
 from django.urls import path
 
+
 app_name='findyourpalapp'
 urlpatterns = [
     # path('findyourpalapp/', views.fyp_login, name='findyourpalapp'),
-    path('fyp_login/', views.fyp_login, name ='fyp_login')
+    path('fyp_login/', views.fyp_login, name ='fyp_login'),
+    path('register/', views.register, name='register'),
 
 ]
--- findyourpalapp/views.py
+++ findyourpalapp/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
 from django.core.mail import send_mail
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 # Create your views here.
 def fyp_login(request):
@@ -9,3 +11,14 @@ def fyp_login(request):
 
     }
     return render(request, 'findyourpalapp/fyp_login.html', context)
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Welcome {username}, your account is created')
+            return redirect('findyourpalapp:fyp_login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'findyourpalapp/register.html', {'form':form})
