09e91394ba98a41bbc55156c69999fd48ca7ab64
San Chua <san.chua@iotamedtech.com>
2022-10-10 21:54:50 +0800

    styled login logout and register page

--- findyourpalapp/forms.py
+++ findyourpalapp/forms.py
@@ -3,8 +3,15 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 class RegisterForm(UserCreationForm):
-    email = forms.EmailField()
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
 
     class Meta:
         model = User
         fields = ['username','email','password1','password2']
+
+    def __init__(self, *args, **kwargs):
+        super(RegisterForm, self).__init__(*args, **kwargs)
+
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
